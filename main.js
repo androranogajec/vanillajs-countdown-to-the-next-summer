7b2e7381b4a205444d50f354539d6bc4a780445f
Andro Ranogajec <ranogaet@gmail.com>
2021-09-17 10:00:40 +0300

    micro edits on styles

--- main.js
+++ main.js
@@ -8,20 +8,16 @@ const SET_FUTURE_DATE = "1 Jun 2022";
 function countdown() {
   const FUTURE_DATE = new Date(SET_FUTURE_DATE);
   const CURRENT_DATE = new Date();
-  let totalSeconds = (FUTURE_DATE - CURRENT_DATE) / 1000; //миллисекунды 
-                                     //в часы /в дни
-  let days = Math.floor(totalSeconds / 3600 / 24);
-                                            //остаток деления на дни
+  let totalSeconds = (FUTURE_DATE - CURRENT_DATE) / 1000; 
+  let days = Math.floor(totalSeconds / 3600 / 24);                              
   let hours = Math.floor(totalSeconds / 3600) % 24;
   let minutes = Math.floor(totalSeconds / 60) % 60;
   let seconds = Math.floor(totalSeconds) % 60;
-  
   daysElement.innerHTML = days
   hoursElement.innerHTML = hours
   minutesElement.innerHTML = minutes
   secondsElement.innerHTML = seconds
-  console.log('seconds',totalSeconds)
-  console.log('milliseconds', FUTURE_DATE - CURRENT_DATE)
+
 }
 setInterval(countdown,1000)
 countdown()
